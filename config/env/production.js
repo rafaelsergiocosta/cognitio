d19ff312cb2fc378fc55683b4e4d325ac17c30c2
Rafael Sergio da Costa <rafael.sergio@dotgroup.com.br>
2017-09-05 10:05:45 -0300

    Openshift test

--- config/env/production.js
+++ config/env/production.js
@@ -1,10 +1,10 @@
 module.exports = {
     env: 'production',
-    db: process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT +
-        'cognitio',
+    db: process.env.MONGODB_USER + ":" +
+        process.env.MONGODB_PASSWORD + "@" +
+        process.env.MONGODBDB_HOST + ':' +
+        process.env.MONGODBDB_PORT + '/' +
+        process.env.MONGODB_DATABASE,
     github: {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
